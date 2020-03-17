748d3d7640db026ce4bec8a445c6464e17f9bf62
Maxim <atomica9999@gmail.com>
2020-03-17 02:36:29 +0200

    minor changes

--- configs/.config/wal/templates/tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "{color1}";
-static const char* selfgcolor   = "{color7}";
-static const char* normbgcolor  = "{color0}";
-static const char* normfgcolor  = "{color7}";
-static const char* urgbgcolor   = "{color8}";
-static const char* urgfgcolor   = "{color0}";
