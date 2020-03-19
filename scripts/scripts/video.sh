45751243ce98c2f1d7674a1ceebe5c4d73fec5aa
Maxim <atomica9999@gmail.com>
2020-03-20 01:06:08 +0200

    menu for local files added

--- scripts/scripts/video.sh
+++ scripts/scripts/video.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
 
-choice=$( (echo 1 | grep 0) | dmenu -p "paste a link to a video  ")
-if [[ $choice == '' ]]; then
-  notify-send 'no input' -t 1000 
-  exit 1
-else
-  mpv --ytdl-format="[height=720]" $choice
-fi
+MENU=$(echo -e 'link\nlocal' | dmenu -p 'link/local:  ')
+
+local_storage () {
+	DIR="$HOME/movies/"
+	CHOSEN="$(ls $DIR | dmenu -l 5 -p 'select video  ')"
+	if [[ -n "$CHOSEN" ]]; then
+		mpv "$DIR$CHOSEN"
+	else
+		exit 0
+	fi
+}
+
+by_link () {
+	choice=$( (echo 1 | grep 0) | dmenu -p "paste a link to a video  ")
+	if [[ -n $choice ]]; then
+		mpv --ytdl-format="[height=720]" $choice
+	else
+		exit 0
+	fi
+}
+
+case $MENU in
+	link)
+		by_link
+		;;
+	local)
+		local_storage
+		;;
+esac
