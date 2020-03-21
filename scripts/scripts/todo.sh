abc7272ff280e9f368837dc1c657940698a27504
Maxim <atomica9999@gmail.com>
2020-03-21 19:28:38 +0200

    minor changes

--- scripts/scripts/todo.sh
+++ scripts/scripts/todo.sh
@@ -3,7 +3,7 @@
 # Simple To-Do script for dmenu
 # with 'add' and 'delete' options.
 
-MAIN=$(echo -e 'add\nlist\ndelete all' | dmenu -p 'to-do  ')
+MAIN=$(echo -e 'add\nlist\ndelete all' | dmenu -p 'to-do ')
 TODO="$HOME/.todo/todo.txt"
 LISTSEL="$HOME/.todo/listsel.txt"
 ADDED="$HOME/.todo/added.txt"
@@ -21,7 +21,7 @@ if [[ $MAIN = 'add' ]]; then
 		mkdir $HOME/.todo/
 	fi
 
-	echo '' | grep 0 | dmenu -p 'add to-do  ' >> $ADDED
+	echo '' | grep 0 | dmenu -p 'add to-do ' >> $ADDED
 
 	if [[ -s $ADDED ]]; then
 		notify-send "$(echo -e "to-do added:\n $(cat $ADDED)")" -t 2000
@@ -31,19 +31,24 @@ if [[ $MAIN = 'add' ]]; then
 	fi
 	
 elif [[ $MAIN = 'list' ]]; then
-	echo -n $(echo -e "$(cat $TODO)" | grep -v -e '^$' | dmenu -l 5 -p 'list  ') > $LISTSEL
+	if [[ -s $TODO ]]; then
+		echo -n $(echo -e "$(cat $TODO)" | grep -v -e '^$' | dmenu -l 5 -p 'list ') > $LISTSEL
 
-	if [[ -s "$LISTSEL" ]]; then
-		DELLIST=$(echo -e 'no\nyes' | dmenu -p 'delete?  ')
-		if [[ $DELLIST = 'yes' ]]; then
-			DEL=$(cat $LISTSEL)
-			sed -i "/$DEL/d" $TODO
-			notify-send "$(echo -e "to-do deleted:\n $(cat $LISTSEL)")" -t 2000
-		else
-			exit 0
+		if [[ -s "$LISTSEL" ]]; then
+			DELLIST=$(echo -e 'no\nyes' | dmenu -p 'delete?  ')
+			if [[ $DELLIST = 'yes' ]]; then
+				DEL=$(cat $LISTSEL)
+				sed -i "/$DEL/d" $TODO
+				notify-send "$(echo -e "to-do deleted:\n $(cat $LISTSEL)")" -t 2000
+			else
+				exit 0
+			fi
 		fi
+		rm $LISTSEL &> /dev/null
+	else
+		echo "(´。＿。｀)" | dmenu -p "it's empty here "
+		exit 0
 	fi
-	rm $LISTSEL &> /dev/null
 
 elif [[ $MAIN = 'delete all' ]]; then
 	confirm
