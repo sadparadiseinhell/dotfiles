56c6c31e8c7da5abc13929289b90b80966ce4e3b
Maxim <atomica9999@gmail.com>
2020-03-22 16:53:06 +0200

    minor changes

--- scripts/scripts/install.sh
+++ scripts/scripts/install.sh
@@ -1,70 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-PP=$(echo -e "xorg-server \nxorg-apps \nxorg-xinit \npython-pywal \nleafpad \nalsa-utils \npulseaudio \nlibnotify \nchromium \npicom \npcmanfm \nsxiv \ndunst \npython-dbus \nmpd \nncmpcpp \nranger \nttf-font-awesome \nfeh \nnetworkmanager \nmaim \nttf-hack \nadapta-gtk-theme \ngit \ntumbler \nffmpegthumbnailer \ntransmission-gtk \nmpv \nttf-dejavu \ntmux \nttf-roboto \nttf-droid \nmpc \nyoutube-dl \ncurl \npamixer \nfortune-mod \nlxappearance \npacman-contrib \nxarchiver \nunzip \nman \nxclip \nxautolock \nstow \ntree \nw3m \nvim \nbc \nxdotool \ntelegram-desktop \nttf-opensans" | sort)
+PP=$(echo -e "xorg-server \nxorg-apps \nxorg-xinit \npython-pywal \nleafpad \nalsa-utils \npulseaudio \nlibnotify \nchromium \npicom \npcmanfm \nsxiv \ndunst \npython-dbus \nmpd \nncmpcpp \nttf-font-awesome \nfeh \nnetworkmanager \nmaim \nttf-hack \nadapta-gtk-theme \ngit \ntumbler \nffmpegthumbnailer \ntransmission-gtk \nmpv \nttf-dejavu \ntmux \nttf-roboto \nttf-droid \nmpc \nyoutube-dl \ncurl \npamixer \nfortune-mod \nlxappearance \npacman-contrib \nxarchiver \nunzip \nman \nxclip \nxautolock \nstow \ntree \nw3m \nvim \nbc \nxdotool \ntelegram-desktop \nttf-opensans" | sort)
 
-PA=$(echo -e "skb \nspotify \nsublime-text-dev \npaper-icon-theme-git \nchromium-widevine \nclipmenu" | sort)
-
-
-if [ -x "$(command -v tput)" ]; then
-	bold="$(tput bold)"
-	black="$(tput setaf 0)"
-	red="$(tput setaf 1)"
-	green="$(tput setaf 2)"
-	yellow="$(tput setaf 3)"
-	blue="$(tput setaf 4)"
-	magenta="$(tput setaf 5)"
-	cyan="$(tput setaf 6)"
-	white="$(tput setaf 7)"
-	grey="$(tput setaf 8)"
-	reset="$(tput sgr0)"
-fi
-
-rbr="${reset}${bold}${red}"
-rbg="${reset}${bold}${green}"
-rby="${reset}${bold}${yellow}"
-rbb="${reset}${bold}${blue}"
-rbm="${reset}${bold}${magenta}"
-rbc="${reset}${bold}${cyan}"
-rbw="${reset}${bold}${white}"
-rbgrey="${reset}${bold}${grey}"
-b="${bold}"
-r="${reset}"
-
-
-folders () {
-	read -p "$(echo -e "Create folders for: ${rbb}music${r}, ${rbr}movies${r}, ${rbg}screenshots${r}? (y/${bold}N${reset}): ")" choice
-	if [[ "$choice" = [Yy] ]]; then
-		mkdir $HOME/{music,movies,screenshots}
-		echo -e "${rby}\nDONE${reset}\n"
-	else
-		echo -e "\n${rbr}~${reset}\n"
-	fi
-}
-
-dotfiles () {
-	read -p "$(echo -e "Download dotfiles from ${bold}${blue}GitHub${reset}? (y/${bold}N${reset}): ")" choice
-	if [[ "$choice" = [Yy] ]]; then
-		cd $HOME/
-    	git clone https://github.com/sadparadiseinhell/dotfiles.git
-    	git clone https://github.com/sadparadiseinhell/sadparadiseinhell.github.io.git
-    	cd ..
-    	echo -e "\n${rby}DONE${reset}\n"
-	else
-		echo -e "\n${rbr}~${reset}\n"
-	fi
-}
+PA=$(echo -e "skb \nsublime-text-dev \npaper-icon-theme-git \nchromium-widevine" | sort)
 
 pcmnpackages () {
-	read -p "$(echo -e "Install base packages (${bold}${yellow}pacman${reset})? (y/${bold}N${reset}): ")" choice
+	read -p "$(echo -e "Install base packages (pacman)? (y/N): ")" choice
 	if [[ "$choice" = [Yy] ]]; then
-		read -p "$(echo -e "\nShow package list? (y/${bold}N${reset}): ")" choice
+		read -p "$(echo -e "\nShow package list? (y/N): ")" choice
 		if [[ "$choice" = [Yy] ]]; then
-			echo -e "\n${bold}${green}$PP${reset}"
-			read -p "$(echo -e "\nEdit package list? (y/${bold}N${reset}): ")" choice
+			echo -e "\n$PP"
+			read -p "$(echo -e "\nEdit package list? (y/N): ")" choice
 
 			if [[ "$choice" = [Yy] ]]; then
 				echo "$PP" > /tmp/packagelist.txt
-				read -p "$(echo -e "${bold}\nEditor:${reset} ")" editor
+				read -p "$(echo -e "\nEditor: ")" editor
 				if [ -n "$editor" ]; then
 					$editor /tmp/packagelist.txt
 				else
@@ -76,27 +26,27 @@ pcmnpackages () {
 				sudo pacman -S $(echo "$PP")
 				rm /tmp/packagelist.txt &> /dev/null		
 			fi
-			echo -e "${rby}DONE${reset}\n"
+			echo -e "${rby}\nDONE${reset}\n"
 		else
 			sudo pacman -S $(echo "$PP")
 			rm /tmp/packagelist.txt &> /dev/null
-			echo -e "\n${rby}DONE${reset}\n"
+			echo -e "\nDONE\n"
 		fi
 	else
-		echo -e "\n${rbr}~${reset}\n"
+		echo -e "\n~\n"
 	fi
 }
 
 aurpackages () {
-	read -p "$(echo -e "Install packages (${rbb}AUR${reset})? (y/${bold}N${reset}): ")" choice
+	read -p "$(echo -e "Install packages (AUR)? (y/N): ")" choice
 	if [[ "$choice" = [Yy] ]]; then
-		read -p "$(echo -e "\nShow package list? (y/${bold}N${reset}): ")" choice
+		read -p "$(echo -e "\nShow package list? (y/N): ")" choice
 		if [[ "$choice" = [Yy] ]]; then
-			echo -e "\n${bold}${green}$PA${reset}"
-			read -p "$(echo -e "\nEdit package list? (y/${bold}N${reset}): ")" choice
+			echo -e "\n$PA"
+			read -p "$(echo -e "\nEdit package list? (y/}N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				echo "$PA" > /tmp/packagelistaur.txt
-				read -p "$(echo -e "${bold}\nEditor:${reset} ")" editor
+				read -p "$(echo -e "\nEditor: ")" editor
 				if [ -n "$editor" ]; then
 					$editor /tmp/packagelistaur.txt
 				else
@@ -108,44 +58,35 @@ aurpackages () {
 				yay -S $(echo "$PA")
 				rm /tmp/packagelistaur.txt &> /dev/null		
 			fi
-			echo -e "\n${rby}DONE${reset}\n"
+			echo -e "\nDONE\n"
 		else
 			yay -S $(echo "$PA")
 			rm /tmp/packagelistaur.txt &> /dev/null
-			echo -e "\n${rby}DONE${reset}\n"
+			echo -e "\nDONE\n"
 		fi
 	else
-		echo -e "\n${rbr}~${reset}\n"
+		echo -e "\n~\n"
 	fi
 }
 
-dots () {
+dotfiles () {
 
-	if [ "$(fortune &> /dev/null; echo $?)" -eq 0 ]; then
-		echo -e "${bold}Before we start:${reset}\n${cyan}$(fortune)${reset}"
-	fi
+	echo
+	echo '------------'
+	echo '| DOTFILES |'
+	echo '------------'
 
-	echo -e "${bold}\n----------------------------------------------"
-	echo "|                                            |"
-	echo "|                  HELLO!                    |"
-	echo "|          This script will copy             |"
-	echo "|    dotfiles from the GitHub repository     |"
-	echo "|              to your system                |"
-	echo "|                                            |"
-	echo -e "----------------------------------------------\n${reset}"
-
-
-	read -p "$(echo -e "Clone ${rbb}dotfiles${r} and ${rbr}startpage${r} from ${rbg}GitHub${r} repository? (y/${b}N${r}): ")" choice
+	read -p "$(echo -e "\nClone dotfiles and startpage from GitHub repository? (y/N): ")" choice
 	if [[ "$choice" = [Yy] ]]; then
 		cd $HOME/
 	    git clone https://github.com/sadparadiseinhell/dotfiles.git
 	    git clone https://github.com/sadparadiseinhell/sadparadiseinhell.github.io.git
-	    echo -e "\n${rby}REPOSITORIES SUCCESSFULLY CLONED${reset}\n"
+	    echo -e "\nREPOSITORIES SUCCESSFULLY CLONED\n"
 	else
-		echo -e "\n${rbr}~${reset}\n"
+		echo -e "\n~\n"
 	fi
 
-	echo -e "Make symlinks for ${rbb}all dotfiles${reset} or ${rbg}individually${r}?\n"
+	echo -e "Make symlinks for all dotfiles or individually?\n"
 
 	OPTIONS="$(echo -e 'All \nIndividually \nSkip')"
 	select opt in $OPTIONS; do
@@ -180,13 +121,13 @@ dots () {
 			mv $HOME/README.md $HOME/dotfiles/
 
 			sudo cp $HOME/dotfiles/configs/.config/wal/colors-wal-dwm.h /usr/lib/python3.8/site-packages/pywal/templates/
-			echo -e "\n${rby}DONE${reset}\n"
+			echo -e "\nDONE\n"
 
 			break
 			
 		elif [[ "$opt" = "Individually" ]]; then
 
-			read -p "$(echo -e "\nInstall ${rbb}DWM${r}? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "\nInstall dwm? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow suckless/
@@ -215,95 +156,94 @@ dots () {
 
 				cd $HOME
 
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n"
+				echo -e "\n~\n"
 			fi
 
-			read -p "$(echo -e "Make symlinks for ${rbb}bash${reset} dotfiles? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "Make symlinks for bash dotfiles? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow bash/
 
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n" 
+				echo -e "\n~\n" 
 			fi
 
-			read -p "$(echo -e "Make symlinks for ${rbb}X${reset} dotfiles? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "Make symlinks for X dotfiles? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow x/
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n"
+				echo -e "\n~\n"
 			fi
 
-			read -p "$(echo -e "Make symlinks for ${rbb}vim${reset} dotfiles? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "Make symlinks for vim dotfiles? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow vim/
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n"
+				echo -e "\n~\n"
 			fi
 
-			read -p "$(echo -e "Make symlinks for ${rbb}tmux${reset} dotfiles? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "Make symlinks for tmux dotfiles? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow tmux/
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n"
+				echo -e "\n~\n"
 			fi
 
-			read -p "$(echo -e "Make symlinks for ${rbb}scripts${reset} folder? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "Make symlinks for scripts folder? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow scripts/
 
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n"
+				echo -e "\n~\n"
 			fi
 
-			read -p "$(echo -e "Make symlinks for ${rbb}wallpapers${reset} folder? (y/${bold}N${reset}): ")" choice
+			read -p "$(echo -e "Make symlinks for wallpapers folder? (y/N): ")" choice
 			if [[ "$choice" = [Yy] ]]; then
 				cd $HOME/dotfiles/
 				stow images/
-				echo -e "\n${rby}DONE${reset}\n"
+				echo -e "\nDONE\n"
 			else
-				echo -e "\n${rbr}~${reset}\n"
+				echo -e "\n~\n"
 			fi
 
 			break
 
 		elif [[ "$opt" = "Skip" ]]; then
-			echo -e "\n${rbr}~${reset}"
+			echo -e "\n~"
 			break
 		else
-			echo -e "\n${rbr}~${reset}" 
+			echo -e "\n~" 
 		fi
 	done
 
 }
-
 
 while [ -n "$1" ]; do
 	case "$1" in
 		-help)
 			echo -e "install.sh usage: \n\tinstall.sh [options ...] \
 			\n\nCommand line options: \
-			\n\t${b}-help${r}\t\tShow this help message. \
-			\n\t${b}-dotfiles${r}\tDownload and copy only dotfiles. \
-			\n\t${b}-packages${r}\tInstall only packages from the official repositories and AUR. \
-			\n\t${b}-list${r}\t\tShow a list of packages to be installed. \
-			\n\t${b}-about${r}\t\tSome information about this script."
+			\n\t-help\t\tShow this help message. \
+			\n\t-dotfiles\tDownload and copy only dotfiles. \
+			\n\t-packages\tInstall only packages from the official repositories and AUR. \
+			\n\t-list\t\tShow a list of packages to be installed. \
+			\n\t-about\t\tSome information about this script."
 			exit	;;
 		-dotfiles)
 			clear
-			dots
-			echo -e "${bold}${red}\nThat's all\n${reset}"
+			dotfiles
+			echo -e "\nThat's all\n"
 			exit	;;
 		-packages)
 			pcmnpackages
@@ -311,89 +251,69 @@ while [ -n "$1" ]; do
 			exit ;;
 		-list)
 			echo -e "\
-			\n${rby}Official repositories:${r} \
+			\nOfficial repositories: \
 			\n$PP \
-			\n\n${rbb}AUR:${r} \
+			\n\nAUR: \
 			\n$PA"
 			exit ;;
 		-about)
-			echo -e "${b}\nThis script will help download dotfiles from my GitHub repository and install basic packages for normal use of the system.${r}\n"
+			echo -e "\nThis script will help download dotfiles from my GitHub repository and install basic packages for normal use of the system.\n"
 			exit ;;
 		*)
 			echo "$1 is not an option"
-			exit	;;
+			exit ;;
 	esac
 	shift
 done
 
 clear
 
-echo -e "${bold}${red}Let's start\n${reset}"
+echo -e "Let's start\n"
 
+pcmnpackages    # Install packages from official repositories
 
-# Pacman #
-
-pcmnpackages
-
-
-# VirtualBox #
-
-read -p "$(echo -e "Install ${rbb}VirtualBox${reset} additions? (y/${bold}N${reset}): ")" choice
+read -p "$(echo -e "Install VirtualBox additions? (y/N): ")" choice    # Install VirtualBox additions
 if [[ "$choice" = [Yy] ]]; then
     sudo pacman -S xf86-video-vmware virtualbox-guest-utils virtualbox-guest-modules-arch --noconfirm
-    echo -e "\n${rby}DONE${reset}\n"
+    echo -e "\nDONE\n"
 else
-	echo -e "\n${rbr}~${reset}\n"
+	echo -e "\n~\n"
 fi
 
-
-# yay #
-
-read -p "$(echo -e "Install ${rby}yay${reset}? (y/${bold}N${reset}): ")" choice
+read -p "$(echo -e "Install yay? (y/N): ")" choice    # Install yay
 if [[ "$choice" = [Yy] ]]; then
 	git clone https://aur.archlinux.org/yay.git
 	cd yay
 	makepkg -si --noconfirm
 	cd ..
 	rm -rf yay/
-	echo -e "\n${rby}DONE${reset}\n"
+	echo -e "\nDONE\n"
 else
-	echo -e "\n${rbr}~${reset}\n"
+	echo -e "\n~\n"
 fi
 
+aurpackages     # Install packages from AUR
 
-# AUR #
-
-aurpackages
-
-
-# NVIDIA #
-
-read -p "$(echo -e "Install ${rbg}NVIDIA${reset} proprietary driver? (y/${bold}N${reset}): ")" choice
+read -p "$(echo -e "Install NVIDIA proprietary driver? (y/N): ")" choice    # Install NVIDIA driver
 if [[ "$choice" = [Yy] ]]; then
     yay -S nvidia-340xx-dkms --noconfirm
-    echo -e "\n${rby}DONE${reset}\n"
+    echo -e "\nDONE\n"
 else
-	echo -e "\n${rbr}~${reset}\n"
+	echo -e "\n~\n"
 fi
 
+read -p "$(echo -e "Create folders for music, movies, screenshots? (y/N): ")" choice    # Create directories
+if [[ "$choice" = [Yy] ]]; then
+	mkdir $HOME/{music,movies,screenshots}
+	echo -e "\nDONE\n"
+fi
 
-# Create directories #
+dotfiles    # Clone and install dotfiles
 
-folders
-
-
-# Dotfiles #
-
-dots
-
-
-# Wallpaper #
-
-read -p "$(echo -e "\nSet ${rbb}wallpaper${reset}? (y/${bold}N${reset}): ")" choice
+read -p "$(echo -e "\nSet wallpaper? (y/N): ")" choice    # Set wallpaper
 if [[ "$choice" = [Yy] ]]; then
 	wal -q -s -t -i $HOME/wallpapers/
-	echo -e "\n${rby}DONE${reset}"
+	echo -e "\nDONE"
 fi
 
-echo -e "${bold}${red}\nThat's all\n${reset}"
+echo -e "\nThat's all\n"
