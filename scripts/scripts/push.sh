ea0c8e0cff29f462c8377af12c16cec4905ea113
Maxim <atomica9999@gmail.com>
2020-03-23 00:36:54 +0200

    scripts folder deleted

--- scripts/scripts/push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd $HOME/dotfiles/
-git add -A
-git commit -m 'minor changes'
-git push
-echo -e "\nDone!\n"
-cd $HOME
--- scripts/scripts/todonotify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LIST="$HOME/.todo/todo.txt"
-
-if [[ -s "$LIST" ]]; then
-	notify-send "$(echo -e "to-do:\n$(cat $LIST)")" -u critical -t 3500
-else
-	exit 0
-fi
