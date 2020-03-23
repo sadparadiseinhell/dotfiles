ca2edd865fd69421ebcbb566c8856c52dc816ca8
Maxim <atomica9999@gmail.com>
2020-03-23 21:14:42 +0200

    minor changes

--- bin/scripts/dwmautostart.sh
+++ bin/scripts/dwmautostart.sh
@@ -3,12 +3,11 @@
 $HOME/scripts/statusbar.sh &                                              # bar
 picom --config $HOME/.config/picom.conf &                                 # composite manager
 setxkbmap -model pc105 -layout us,ru,ua -option grp:ctrl_alt_toggle &     # keyboard layout
-dunst -config $HOME/.cache/wal/dwm-dunstrc &                              # notification daemon
+dunst -config $HOME/.config/dunst/dunstrc &                               # notification daemon
 xautolock -time 10 -detectsleep -locker "$HOME/scripts/lock.sh" \
 -notify 10 -notifier "notify-send --urgency normal --expire-time=3500 \
 'locking screen in 10 seconds'" -corners +-00 -cornerdelay 10 \
 -cornerredelay 10 -cornersize 20 &                                        # automatic screen lock
-clipmenud &                                                               # clipboard manager
 
 if [ "$(ps -aux | grep [m]pd | wc -l)" -eq 0 ]; then                      #
 	mpd                                                                   #
