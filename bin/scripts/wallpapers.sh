c41c042c55bda0b7cbb150b8ea5fa23d1d30a754
Maxim <atomica9999@gmail.com>
2020-03-24 22:47:00 +0200

    minor changes

--- bin/scripts/wallpapers.sh
+++ bin/scripts/wallpapers.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-while true; do
-	WPLIGHT="$HOME/wallpapers/nord-light-solid.png"
-	WPDARK="$HOME/wallpapers/nord-solid.png"
-	TIME=$(date +%H)
+TIME=$(date +%H)
+FILE='/tmp/color.png'
+#POLARNIGHT='#2E3440 #3B4252 #434C5E #4C566A'
+#SNOWSTORM='#D8DEE9 #E5E9F0 #ECEFF4'
 
+while true; do
 	if [[ $TIME -lt 06 ]]; then
-		feh --bg-scale $WPDARK
+		convert -size 32x32 xc:#2E3440 $FILE
 	elif [[ $TIME -lt 18 ]]; then
-		feh --bg-scale $WPLIGHT
+		convert -size 32x32 xc:#D8DEE9 $FILE
+	elif [[ $TIME -lt 21 ]]; then
+		convert -size 32x32 xc:#3B4252 $FILE
 	elif [[ $TIME -lt 24 ]]; then
-		feh --bg-scale $WPDARK
+		convert -size 32x32 xc:#2E3440 $FILE
 	fi
+	feh --bg-tile $FILE
 	sleep 10m
 done
