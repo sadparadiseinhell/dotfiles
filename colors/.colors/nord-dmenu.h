7e34cb585746993726eb4b5dbee8d81b9eb18d73
Maxim <atomica9999@gmail.com>
2020-03-27 21:29:37 +0200

    deleted

--- colors/.colors/nord-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*               fg         bg       */
-	[SchemeNorm] = { "#D8DEE9", "#2E3440" },
-	[SchemeSel]  = { "#ECEFF4", "#81A1C1" },
-	[SchemeOut]  = { "#D8DEE9", "#8FBCBB" },
-};
