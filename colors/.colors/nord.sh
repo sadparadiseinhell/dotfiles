ffebf033a3dc1647bc3bf67ae06a3bca9060bd38
Maxim <atomica9999@gmail.com>
2020-04-07 14:25:01 +0300

    new colorschemes added

---
+++ colors/.colors/nord.sh
@@ -0,0 +1,28 @@
+#2E3440 #3B4252 #434C5E #4C566A          # Polar Night
+#D8DEE9 #E5E9F0 #ECEFF4                  # Snow Storm
+#8FBCBB #88C0D0 #81A1C1 #5E81AC          # Frost
+#BF616A #D08770 #EBCB8B #A3BE8C #B48EAD  # Aurora
+
+# Normal colors
+color0='#3b4252'  # black
+color1='#bf616a'  # red
+color2='#a3be8c'  # green
+color3='#ebcb8b'  # yellow
+color4='#81a1c1'  # blue
+color5='#b48ead'  # magenta
+color6='#88c0d0'  # cyan
+color7='#e5e9f0'  # white
+
+# Bright colors
+color8='#4c566a'  # black
+color9='#bf616a'  # red
+color10='#a3be8c' # green
+olor11='#ebcb8b' # yellow
+color12='#81a1c1' # blue
+color13='#b48ead' # magenta
+color14='#8fbcbb' # cyan
+color15='#eceff4' # white
+
+light='#D8DEE9'
+dark='#3B4252'
+darker='#2E3440'
