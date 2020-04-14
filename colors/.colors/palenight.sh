40947bab54c7134f531827253d715098c88f63c7
Maxim <atomica9999@gmail.com>
2020-04-14 18:01:59 +0300

    new color schemes

---
+++ colors/.colors/palenight.sh
@@ -0,0 +1,23 @@
+# Normal colors
+color0='#292d3e'  # black
+color1='#f07178'  # red
+color2='#c3e88d'  # green
+color3='#ffcb6b'  # yellow
+color4='#82aaff'  # blue
+color5='#c792ea'  # magenta
+color6='#89ddff'  # cyan
+color7='#959dcb'  # white
+
+# Bright colors
+color8='#676e95'  # black
+color9='#f78c6c'  # red
+color10='#444267' # green
+color11='#32374d' # yellow
+color12='#8796b0' # blue
+color13='#959dcb' # magenta
+color14='#ff5370' # cyan
+color15='#ffffff' # white
+
+light='#8796b0'
+dark='#676e95'
+darker='#292d3e'
