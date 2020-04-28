7ce65d9f319d947e6ada47fdd7f4bc22b95c7652
Maxim <atomica9999@gmail.com>
2020-04-28 22:39:02 +0300

    28/04/20 22:39

---
+++ bin/scripts/drawbar.sh
@@ -0,0 +1,51 @@
+STEPS=$1
+LENGHT=$2
+DRAWEMPTY=$3
+
+FULLCHAR="█"
+MIDCHAR0="▆"
+MIDCHAR1="▅"
+MIDCHAR2="▃"
+MIDCHAR3="▁"
+MIDPOINT=""
+EMPTYCHAR="░"
+
+
+# Line
+#FULLCHAR="━"
+#MIDCHAR0="╾"
+#MIDCHAR1="╾"
+#MIDCHAR2="╾"
+#MIDCHAR3="╾"
+#MIDPOINT=""
+#EMPTYCHAR="─"
+
+# Using braille
+#FULLCHAR="⣿"
+#MIDCHAR0="⡷"
+#MIDCHAR1="⡇"
+#MIDCHAR2="⠆"
+#MIDCHAR3="⠂"
+#MIDPOINT=""
+#EMPTYCHAR="⠀"
+
+if [[ $DRAWEMPTY = false ]]; then
+	BAR=$(seq -s $FULLCHAR 0 $STEPS $LENGHT | sed 's/[0-9]//g') 
+	if [ $(( ($LENGHT-4) % $STEPS )) -eq 0 ]; then
+		MIDBAR=$MIDCHAR0
+	elif [ $(( ($LENGHT-3) % $STEPS )) -eq 0 ]; then
+		MIDBAR=$MIDCHAR1
+	elif [ $(( ($LENGHT-2) % $STEPS )) -eq 0 ]; then
+		MIDBAR=$MIDCHAR2
+	elif [ $(( ($LENGHT-1) % $STEPS )) -eq 0 ]; then
+		MIDBAR=$MIDCHAR3
+	fi
+
+EMPTYBAR=$(seq -s $EMPTYCHAR $(( ((100-$LENGHT) / $STEPS ) +1 )) | sed 's/[0-9]//g')
+FINALBAR="$BAR$MIDBAR$MIDPOINT$EMPTYBAR"
+
+elif [[ $DRAWEMPTY = true ]]; then
+	FINALBAR="$(seq -s $EMPTYCHAR $(($LENGHT/$STEPS +1)) | sed 's/[0-9]//g')"
+fi
+
+echo $FINALBAR
--- bin/scripts/volume.sh
+++ bin/scripts/volume.sh
@@ -1,22 +1,69 @@
 #!/bin/sh
 
+# Enable to play sound
+PLAYSOUND=true
+
+# Sound to play
+SOUND=/usr/share/sounds/freedesktop/stereo/audio-volume-change.oga
+
+# At which level the volume is considered too high
+WARNINGLEVEL=60
+
+# For how much milliseconds the notification will stay visible
+TIMEOUT=1500
+
+# Unique dunst notification id
+ID=2593
+
+
 refreshbar () {
 	kill "$(pstree -lp | grep -- -statusbar.sh\([0-9] | sed "s/.*sleep(\([0-9]\+\)).*/\1/")"
 }
 
+getVolume () {
+    pamixer --get-volume
+}
+
+buildBar () {
+	$HOME/dotfiles/bin/scripts/drawbar.sh $1 $2 $3
+}
+
+sendNotification () {
+	VOLUME=`getVolume`
+	BODY="`buildBar 5 $VOLUME false` $VOLUME%"
+	
+	if [ $VOLUME -ge $WARNINGLEVEL ]; then
+		URGENCY=critical
+	fi
+	
+	if [ $PLAYSOUND = true ]; then
+		paplay $SOUND &
+	fi
+	
+	dunstify -t "$TIMEOUT" -r "$ID" -u "$URGENCY" "$BODY"
+}
+
 case $1 in
 	up)
-	pamixer -u &> /dev/null
-	pamixer -i 5 &> /dev/null
-	refreshbar
+		pamixer -u &> /dev/null
+		pamixer -i 5 &> /dev/null
+		refreshbar
+		sendNotification
 		;;
 	down)
-	pamixer -u &> /dev/null
-	pamixer -d 5 &> /dev/null
-	refreshbar
+		pamixer -u &> /dev/null
+		pamixer -d 5 &> /dev/null
+		refreshbar
+		sendNotification
 		;;
 	mute)
-	pamixer -t &> /dev/null
-	refreshbar
+		pamixer -t &> /dev/null
+		refreshbar
+		if [[ $(pamixer --get-volume-human) = 'muted' ]]; then
+			BODY="`buildBar 5 100 true` Muted"
+			dunstify -t "$TIMEOUT" -r "$ID" -u "$URGENCY" "$BODY"
+		else
+			sendNotification
+		fi
 		;;
 esac
